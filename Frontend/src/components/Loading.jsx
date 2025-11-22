5664b6f0b86c61bf910c6f04cf2e44df05553d01
Sabarish-K-S <sabarish28072006@gmail.com>
2025-11-22 23:37:55 +0530

    Update loading component class names for consistency

--- Frontend/src/components/Loading.jsx
+++ Frontend/src/components/Loading.jsx
@@ -2,9 +2,9 @@ import '../styles/loading.css'
 const Loading = () => {
   return (
     <div className='loader'>
-      <div className='line line1'></div>
-      <div className='line line2'></div>
-      <div className='line line3'></div>
+      <div className='lines line1'></div>
+      <div className='lines line2'></div>
+      <div className='lines line3'></div>
     </div>
   )
 }
